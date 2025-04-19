ded2d971645a9ed04ee977424901129d6431cc2b
garethslinn <gslinn@gmail.com>
2025-04-19 22:35:52 +0100

    apply a skip link, this isn't working right now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <header className="header" role="banner">
+        <a href="#main-content">Skip Link</a>
         <img
             src="https://raw.githubusercontent.com/garethslinn/public_infocharts/refs/heads/main/logo.svg"
             height="100"
